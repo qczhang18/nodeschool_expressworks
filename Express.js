f4763ef483137c56468ef2160f41b631760c3afd
Q. Zhang <aznyqjai@gmail.com>
2017-05-08 01:32:33 -0400

    complete 4

--- Express.js
+++ Express.js
@@ -1,13 +1,22 @@
 var express = require('express')
 var app = express()
 var path = require('path');
+var bodyparser = require('body-parser')
 
+
+app.listen(process.argv[2])
+
+
+app.use(bodyparser.urlencoded({extended: false}))
 
 app.get('/home', function(req, res) {
     res.render('index', {date: new Date().toDateString()})
 })
 
-app.listen(process.argv[2])
+
+app.post('/form', function(req, res) {
+    res.send(req.body.str.split('').reverse().join(''))
+})
 
 
 app.set('views', path.join(__dirname, 'templates'))
@@ -16,4 +25,3 @@ app.set('view engine', 'pug')
 
 
 app.use(express.static(process.argv[3] || path.join(__dirname, 'public')))
-
