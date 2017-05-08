42fd9b1180b66760c7a0b4114401e37285f3bfb9
Q. Zhang <aznyqjai@gmail.com>
2017-05-08 01:06:43 -0400

    complete 3

--- Express.js
+++ Express.js
@@ -1,9 +1,19 @@
 var express = require('express')
 var app = express()
+var path = require('path');
+
+
 app.get('/home', function(req, res) {
-    res.end('Hello World!')
+    res.render('index', {date: new Date().toDateString()})
 })
 
 app.listen(process.argv[2])
 
+
+app.set('views', path.join(__dirname, 'templates'))
+
+app.set('view engine', 'pug')
+
+
 app.use(express.static(process.argv[3] || path.join(__dirname, 'public')))
+
