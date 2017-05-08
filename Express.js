5a05c402a1b89b0d4c197d1c30f587f093d5ee19
Q. Zhang <aznyqjai@gmail.com>
2017-05-08 02:14:30 -0400

    complete 6

--- Express.js
+++ Express.js
@@ -1,11 +1,12 @@
-var express = require('express')
-var app = express()
 var path = require('path');
-var bodyparser = require('body-parser')
+var express = require('express');
+var app = express();
 
+app.put('/message/:id', function (req, res) {
+  var id = req.params.id;
+  var str = require('crypto').createHash('sha1').
+      update(new Date().toDateString().toString() + id).digest('hex');
+  res.send(str);
+});
 
-app.listen(process.argv[2])
-
-app.use(require('stylus').middleware('public'))
-
-app.use(express.static(process.argv[3] || path.join(__dirname, 'public')))
+app.listen(process.argv[2]);
