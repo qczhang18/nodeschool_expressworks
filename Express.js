848abbecc6b038fd6ade589f5f33c28697aac4e5
Q. Zhang <aznyqjai@gmail.com>
2017-05-08 02:22:29 -0400

    complete 7

--- Express.js
+++ Express.js
@@ -1,12 +1,16 @@
+var express = require('express')
+var app = express()
 var path = require('path');
-var express = require('express');
-var app = express();
+var bodyparser = require('body-parser')
 
-app.put('/message/:id', function (req, res) {
-  var id = req.params.id;
-  var str = require('crypto').createHash('sha1').
-      update(new Date().toDateString().toString() + id).digest('hex');
-  res.send(str);
-});
 
-app.listen(process.argv[2]);
+app.listen(process.argv[2])
+
+app.use(bodyparser.urlencoded({extended: false}))
+
+app.get('/search', function (req, res){
+    var results = req.query;
+    res.send(results);
+})
+
+app.use(express.static(process.argv[3] || path.join(__dirname, 'public')))
