4493dc8aeb75a6542c3e1c1125a25cab8cd95620
Q. Zhang <aznyqjai@gmail.com>
2017-05-08 02:36:20 -0400

    complete 8 - all

--- Express.js
+++ Express.js
@@ -1,16 +1,22 @@
 var express = require('express')
 var app = express()
-var path = require('path');
-var bodyparser = require('body-parser')
+var fs = require('fs');
+
+
+
+app.get('/books', function (req, res) {
+  fs.readFile(process.argv[3], function (err, content) {
+    if (err) {
+      res.send(500);
+    }
+    try {
+      var books = JSON.parse(content.toString());
+      res.json(books);
+    } catch (error) {
+      res.send(500);
+    }
+  });
+});
 
 
 app.listen(process.argv[2])
-
-app.use(bodyparser.urlencoded({extended: false}))
-
-app.get('/search', function (req, res){
-    var results = req.query;
-    res.send(results);
-})
-
-app.use(express.static(process.argv[3] || path.join(__dirname, 'public')))
